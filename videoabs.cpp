ed6c549d5579c2a03f5f6dbda90c56f9155ad592
Lucas Amparo <lucasamparo.ti@gmail.com>
2017-03-31 15:40:18 -0300

    Pre finalizado

--- videoabs.cpp
+++ videoabs.cpp
@@ -11,11 +11,11 @@ using namespace std;
 int main( int argc, char** argv ){
 
     	Mat input, output;
-	Mat dog, bilateral, lab, lum, wrap;
+	Mat dog, pre_dog, bilateral, lab, lum, wrap;
 	stringstream ss;
-	ss << "img/imagem1.jpg";
+	ss << "img/imagem0.jpg";
 	input = imread(ss.str().c_str(), CV_LOAD_IMAGE_COLOR);
-	output = input;
+	output = input.clone();
 	Size sz1 = input.size();
 	Size sz2 = output.size();
 
@@ -28,10 +28,16 @@ int main( int argc, char** argv ){
 	cvtColor(input, lab, CV_BGR2Lab);
 
 	//Applying Bilateral Filter
-	bilateralFilter(lab, bilateral, 15, 150, 150);
+	bilateralFilter(input, bilateral, 9, 50, 50);
+	pre_dog = bilateral.clone();
+	for(int i = 0; i < 5; i++){
+		bilateralFilter(bilateral.clone(), bilateral, 9, 50, 50);
+	}
+	//imshow("bilateral", bilateral);
+	cvtColor(bilateral, lab, CV_BGR2Lab);
 
 	//Luminance Quantization
-	int q = 10;
+	int q = 5;
 	for(int i = 0; i < lab.size().height; i++){
 		for(int j = 0; j < lab.size().width; j++){
 			Vec3b value = lab.at<Vec3b>(i,j);
@@ -40,27 +46,49 @@ int main( int argc, char** argv ){
 			lab.at<Vec3b>(i,j) = value;
 		}
 	}
-	cvtColor(lab, lab, CV_Lab2BGR);
-	imshow("depois 1", lab);
+	Mat tmp;
+	cvtColor(lab, tmp, CV_Lab2BGR);
+	//imshow("lab", tmp);
 
 	//Applying Difference of Gaussians
 	Mat g1, g2;
-	GaussianBlur(input,g1,Size(1,1),0);
-	GaussianBlur(input,g2,Size(5,5),0);
-	dog = g1 - g2;
-	dog = Scalar::all(255) - dog;
+	int sub_factor;
+	GaussianBlur(pre_dog,g1,Size(sqrt(1.6),sqrt(1.6)),0);
+	GaussianBlur(pre_dog,g2,Size(25,25),0);
+	dog = g2 - g1;
+	bitwise_not(dog,dog);
+	cvtColor(dog,dog,CV_Lab2BGR);
+	cvtColor(dog,dog,CV_BGR2GRAY);
+	dog = dog < 128;
+	//imshow("dog", dog);
 
 	//Generating presentation
-	addWeighted(dog, 0.5, lab, 0.5, 0.0, output);
+	for(int i = 0; i < lab.size().height; i++){
+		for(int j = 0; j < lab.size().width; j++){
+			if((int)dog.at<uchar>(i,j) == 0){
+				output.at<Vec3b>(i,j) = Vec3b(0,127,127);
+			} else {
+				output.at<Vec3b>(i,j) = lab.at<Vec3b>(i,j);
+			}
+		}
+	}
+	cvtColor(output, output, CV_Lab2BGR);
 
-	Mat image(sz1.height, sz1.width+sz2.width, CV_8UC3);
-	image.adjustROI(0,0,0,-sz2.width);
-	input.copyTo(image);
+	Mat output2 = output.clone();
+	for(int i = 0; i < bilateral.size().height; i++){
+		for(int j = 0; j < bilateral.size().width; j++){
+			if((int)dog.at<uchar>(i,j) == 0){
+				output2.at<Vec3b>(i,j) = Vec3b(0,0,0);
+			} else {
+				output2.at<Vec3b>(i,j) = bilateral.at<Vec3b>(i,j);
+			}
+		}
+	}
+	//cvtColor(output, output, CV_Lab2BGR);
 
-	image.adjustROI(0,0,-sz1.width,sz2.width);
-	output.copyTo(image);
-
-	image.adjustROI(0,0,sz1.width,0);
+	Mat image;
+	hconcat(input, output2, image);
+	hconcat(image, output, image);
    	namedWindow( "Display window", WINDOW_AUTOSIZE );
     	imshow( "Display window", image );
     	
