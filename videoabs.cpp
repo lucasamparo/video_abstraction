f041f737e920c1db24b3ad4e2fc0575b19e578e6
Lucas Amparo <lucasamparo.ti@gmail.com>
2017-03-31 15:59:58 -0300

    Navegação inclusa

--- videoabs.cpp
+++ videoabs.cpp
@@ -9,89 +9,108 @@ using namespace cv;
 using namespace std;
 
 int main( int argc, char** argv ){
+	int count = 0;
+	while(1){
+		Mat input, output;
+		Mat dog, pre_dog, bilateral, lab, lum, wrap;
+		stringstream ss;
+		ss << "img/imagem" << count << ".jpg";
+		input = imread(ss.str().c_str(), CV_LOAD_IMAGE_COLOR);
+		output = input.clone();
+		Size sz1 = input.size();
+		Size sz2 = output.size();
 
-    	Mat input, output;
-	Mat dog, pre_dog, bilateral, lab, lum, wrap;
-	stringstream ss;
-	ss << "img/imagem0.jpg";
-	input = imread(ss.str().c_str(), CV_LOAD_IMAGE_COLOR);
-	output = input.clone();
-	Size sz1 = input.size();
-	Size sz2 = output.size();
-
-	if(! input.data ){
-		cout <<  "Could not open or find the image" << std::endl ;
-		return -1;
-	}
-
-	//RGB to Lab Conversion
-	cvtColor(input, lab, CV_BGR2Lab);
-
-	//Applying Bilateral Filter
-	bilateralFilter(input, bilateral, 9, 50, 50);
-	pre_dog = bilateral.clone();
-	for(int i = 0; i < 5; i++){
-		bilateralFilter(bilateral.clone(), bilateral, 9, 50, 50);
-	}
-	//imshow("bilateral", bilateral);
-	cvtColor(bilateral, lab, CV_BGR2Lab);
-
-	//Luminance Quantization
-	int q = 5;
-	for(int i = 0; i < lab.size().height; i++){
-		for(int j = 0; j < lab.size().width; j++){
-			Vec3b value = lab.at<Vec3b>(i,j);
-			float tax = value.val[0] / 255.0;
-			value.val[0] = (round(tax * q)/q)*255;
-			lab.at<Vec3b>(i,j) = value;
+		if(! input.data ){
+			cout <<  "Could not open or find the image" << std::endl ;
+			return -1;
 		}
-	}
-	Mat tmp;
-	cvtColor(lab, tmp, CV_Lab2BGR);
-	//imshow("lab", tmp);
 
-	//Applying Difference of Gaussians
-	Mat g1, g2;
-	int sub_factor;
-	GaussianBlur(pre_dog,g1,Size(sqrt(1.6),sqrt(1.6)),0);
-	GaussianBlur(pre_dog,g2,Size(25,25),0);
-	dog = g2 - g1;
-	bitwise_not(dog,dog);
-	cvtColor(dog,dog,CV_Lab2BGR);
-	cvtColor(dog,dog,CV_BGR2GRAY);
-	dog = dog < 128;
-	//imshow("dog", dog);
+		//RGB to Lab Conversion
+		cvtColor(input, lab, CV_BGR2Lab);
 
-	//Generating presentation
-	for(int i = 0; i < lab.size().height; i++){
-		for(int j = 0; j < lab.size().width; j++){
-			if((int)dog.at<uchar>(i,j) == 0){
-				output.at<Vec3b>(i,j) = Vec3b(0,127,127);
-			} else {
-				output.at<Vec3b>(i,j) = lab.at<Vec3b>(i,j);
+		//Applying Bilateral Filter
+		bilateralFilter(input, bilateral, 9, 50, 50);
+		pre_dog = bilateral.clone();
+		for(int i = 0; i < 5; i++){
+			bilateralFilter(bilateral.clone(), bilateral, 9, 50, 50);
+		}
+		//imshow("bilateral", bilateral);
+		cvtColor(bilateral, lab, CV_BGR2Lab);
+
+		//Luminance Quantization
+		int q = 5;
+		for(int i = 0; i < lab.size().height; i++){
+			for(int j = 0; j < lab.size().width; j++){
+				Vec3b value = lab.at<Vec3b>(i,j);
+				float tax = value.val[0] / 255.0;
+				value.val[0] = (round(tax * q)/q)*255;
+				lab.at<Vec3b>(i,j) = value;
 			}
 		}
-	}
-	cvtColor(output, output, CV_Lab2BGR);
+		Mat tmp;
+		cvtColor(lab, tmp, CV_Lab2BGR);
+		//imshow("lab", tmp);
 
-	Mat output2 = output.clone();
-	for(int i = 0; i < bilateral.size().height; i++){
-		for(int j = 0; j < bilateral.size().width; j++){
-			if((int)dog.at<uchar>(i,j) == 0){
-				output2.at<Vec3b>(i,j) = Vec3b(0,0,0);
-			} else {
-				output2.at<Vec3b>(i,j) = bilateral.at<Vec3b>(i,j);
+		//Applying Difference of Gaussians
+		Mat g1, g2;
+		int sub_factor;
+		GaussianBlur(pre_dog,g1,Size(sqrt(1.6),sqrt(1.6)),0);
+		GaussianBlur(pre_dog,g2,Size(25,25),0);
+		dog = g2 - g1;
+		bitwise_not(dog,dog);
+		cvtColor(dog,dog,CV_Lab2BGR);
+		cvtColor(dog,dog,CV_BGR2GRAY);
+		dog = dog < 128;
+		//imshow("dog", dog);
+
+		//Generating presentation
+		for(int i = 0; i < lab.size().height; i++){
+			for(int j = 0; j < lab.size().width; j++){
+				if((int)dog.at<uchar>(i,j) == 0){
+					output.at<Vec3b>(i,j) = Vec3b(0,127,127);
+				} else {
+					output.at<Vec3b>(i,j) = lab.at<Vec3b>(i,j);
+				}
 			}
 		}
-	}
-	//cvtColor(output, output, CV_Lab2BGR);
+		cvtColor(output, output, CV_Lab2BGR);
 
-	Mat image;
-	hconcat(input, output2, image);
-	hconcat(image, output, image);
-   	namedWindow( "Display window", WINDOW_AUTOSIZE );
-    	imshow( "Display window", image );
-    	
-	waitKey(0);
-	return 0;
+		Mat output2 = output.clone();
+		for(int i = 0; i < bilateral.size().height; i++){
+			for(int j = 0; j < bilateral.size().width; j++){
+				if((int)dog.at<uchar>(i,j) == 0){
+					output2.at<Vec3b>(i,j) = Vec3b(0,0,0);
+				} else {
+					output2.at<Vec3b>(i,j) = bilateral.at<Vec3b>(i,j);
+				}
+			}
+		}
+		//cvtColor(output, output, CV_Lab2BGR);
+
+		Mat image;
+		hconcat(input, output2, image);
+		hconcat(image, output, image);
+	   	namedWindow( "Real Time Video Abstraction", WINDOW_AUTOSIZE );
+	    	imshow( "Real Time Video Abstraction", image );
+	    	
+		int c =	waitKey(0);
+		switch(c){
+			case 81:
+				if(count > 0)
+					count--;
+				else
+					count = 12;
+			break;
+			case 83:
+				if(count < 12)
+					count++;
+				else
+					count = 0;
+			break;
+			case 27:
+				cout << "fechando programa" << endl;
+				return 0;
+			break;
+		}
+	}
 }
